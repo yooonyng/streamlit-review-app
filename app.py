baa3e0101d8b1ded3ed5deebab96c2f67345c9d2
yooonyng <yooonyng@gmail.com>
2022-05-24 14:25:51 +0900

    0524

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from app_home import run_home
 from app_eda import run_eda
+from app_ml import run_ml
 
 def main():
     st.title('리뷰 긍정 부정 예측 앱')
@@ -13,7 +14,7 @@ def main():
     elif choice == menu[1]:
         run_eda()
     elif choice == menu[2]:
-        pass
+        run_ml()    
 
 
 if __name__ == '__main__':
